6fd14e47a67ab48928fae4e8c7d0349fc1c08445
Livedave004 <port6six@gmail.com>
2024-11-19 15:55:46 -0500

    Add files via upload

--- config.php
+++ config.php
@@ -1,8 +1,13 @@
 <?php
-// token de Telegram para el bot
-$token = "7847507216:AAEYNsjqwflBe9f5Zv-3vMLabw1siVFvlto"; // ejemplo del token, colócalo aquí.
+// Evitar el acceso directo al archivo
+if (basename($_SERVER['PHP_SELF']) == basename(__FILE__)) {
+    // Mostrar error 404 en grande y detener la ejecución si alguien accede directamente
+    header("HTTP/1.0 404 Not Found");
+    echo "<h1 style='font-size: 50px; color: red; text-align: center;'>404 Not Found</h1>";
+    exit;
+}
 
-// chat_id de Telegram donde quieres enviar los mensajes
-$chat_id = "5157616506"; // el ID de chat que te dará Telegram
-
+// Aquí están tus datos sensibles
+$token = "7847507216:AAEYNsjqwflBe9f5Zv-3vMLabw1siVFvlto";
+$chat_id = "5157616506";
 ?>
